6ef5d77cc4bd150ba4e47f5c1885d1f8e6ea0597
Rupinder Singh <rupinder.singh@6sense.com>
2021-01-02 19:12:20 +0530

    remove dead config

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
-    // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(path.join(__dirname, '../src/index.html'))
     }),
