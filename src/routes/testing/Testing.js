bb5c4a7dc9dabaed8b4199cc022e9822cf20f8e4
Rupinder Singh <rupinder.singh@6sense.com>
2021-01-03 03:23:55 +0530

    eyeball testing

--- src/routes/testing/Testing.js
+++ src/routes/testing/Testing.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import styles from './Testing.module.css';
 
-const Testing = () => (
-<div>This is Testing</div>
-);
+const keepInRange = (lower, upper, testValue) => {
+  const [absLower, absUpper, absTestValue] = [lower, upper, testValue].map(v => Math.abs(parseInt(v)));
+  if( absTestValue < absLower) return (absLower);
+  if( absTestValue > absUpper) return absUpper;
+
+  return absTestValue;
+}
+
+const Testing = () => {
+  const eyeball = useRef(null);
+  const eye = useRef(null);
+  const playArea = useRef(null);
+
+  const onMouseMove = (e) => {
+    const [pointerLeft, pointerTop] = [e.clientX, e.clientY];
+    const { left: playAreaLeft, top: playAreaTop } = playArea.current.getBoundingClientRect();
+    const { left: eyeLeft, right: eyeRight, top: eyeTop, bottom: eyeBottom } = eye.current.getBoundingClientRect();
+    const { height: eyeballHeight, width: eyeballWidth} = eyeball.current.getBoundingClientRect();
+
+    eyeball.current.style.top = keepInRange(
+      eyeTop - playAreaTop + ((eyeballHeight / 2) + 1),
+      eyeBottom - playAreaTop - ((eyeballHeight / 2) + 3),
+      pointerTop - playAreaTop
+    );
+    eyeball.current.style.left = keepInRange(
+      eyeLeft - playAreaLeft + ((eyeballHeight / 2) + 1),
+      eyeRight - playAreaLeft - ((eyeballHeight / 2) + 3),
+      pointerLeft - playAreaLeft
+    );
+  }
+
+  return (
+    <div className={styles.container} onMouseMove={onMouseMove} ref={playArea}>
+      <div className={styles.eye} ref={eye}>
+        <div className={styles.eyeball} ref={eyeball}>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Testing;
