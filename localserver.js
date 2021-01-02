c0113d734fc7bbd6564452219de22af332abdddd
Rupinder Singh <rupinder.singh@6sense.com>
2021-01-02 16:53:33 +0530

    publish from docs instead of src

--- localserver.js
+++ localserver.js
@@ -5,14 +5,15 @@ var http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const port = 8080
+const sourceDir = './docs';
 
-app.use(express.static('src'))
+app.use(express.static(sourceDir))
 
 http.listen(port, () => {
   console.log(`App running at http://localhost:${port}`)
 })
 
-chokidar.watch('./src').on('all', (event, path) => {
+chokidar.watch(sourceDir).on('all', (event, path) => {
   console.log('chokidar event:', event, path);
   io.emit('reload');
 });
