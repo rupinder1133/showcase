3856f3529620dab646ffe98c9fc5c01e2d2afba1
Rupinder Singh <rupinder.singh@6sense.com>
2021-01-02 18:37:02 +0530

    webpack

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,9 @@
+const baseConfig = require('./webpack.config.base')
+const { merge } = require('webpack-merge');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devServer: {
+    contentBase: './docs',
+  },
+});
